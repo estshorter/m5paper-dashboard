5a7a4b9a363dae876da367c48b17366de21ecf56
estshorter <1430311+estshorter@users.noreply.github.com>
2021-01-31 01:02:16 +0900

    small fix for SHT3X

--- src/SHT3X.cpp
+++ src/SHT3X.cpp
@@ -69,4 +69,40 @@ namespace SHT3X
 
     return 0;
   }
+
+  /**
+ * Performs a CRC8 calculation on the supplied values.
+ *
+ * @param data  Pointer to the data to use when calculating the CRC8.
+ * @param len   The number of bytes in 'data'.
+ *
+ * @return The computed CRC8 value.
+ */
+  uint8_t SHT3X::crc8(const uint8_t *data, const int len)
+  {
+    /*
+   *
+   * CRC-8 formula from page 14 of SHT spec pdf
+   *
+   * Test data 0xBE, 0xEF should yield 0x92
+   *
+   * Initialization data 0xFF
+   * Polynomial 0x31 (x8 + x5 +x4 +1)
+   * Final XOR 0x00
+   */
+
+    constexpr uint8_t POLYNOMIAL(0x31);
+    uint8_t crc(0xFF);
+
+    for (int j = len; j; --j)
+    {
+      crc ^= *data++;
+
+      for (int i = 8; i; --i)
+      {
+        crc = (crc & 0x80) ? (crc << 1) ^ POLYNOMIAL : (crc << 1);
+      }
+    }
+    return crc;
+  }
 } // namespace SHT3X
--- src/SHT3X.h
+++ src/SHT3X.h
@@ -1,32 +1,4 @@
 #pragma once
-/*
-Software License Agreement (BSD License)
-
-Copyright (c) 2012, Adafruit Industries
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-1. Redistributions of source code must retain the above copyright
-notice, this list of conditions and the following disclaimer.
-2. Redistributions in binary form must reproduce the above copyright
-notice, this list of conditions and the following disclaimer in the
-documentation and/or other materials provided with the distribution.
-3. Neither the name of the copyright holders nor the
-names of its contributors may be used to endorse or promote products
-derived from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ''AS IS'' AND ANY
-EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
 
 #include "Arduino.h"
 #include "Wire.h"
@@ -61,40 +33,6 @@ namespace SHT3X
     float _cTemp = 0;
     uint_fast8_t _humidity = 0;
 
-    /**
- * Performs a CRC8 calculation on the supplied values.
- *
- * @param data  Pointer to the data to use when calculating the CRC8.
- * @param len   The number of bytes in 'data'.
- *
- * @return The computed CRC8 value.
- */
-    static uint8_t crc8(const uint8_t *data, const int len)
-    {
-      /*
-   *
-   * CRC-8 formula from page 14 of SHT spec pdf
-   *
-   * Test data 0xBE, 0xEF should yield 0x92
-   *
-   * Initialization data 0xFF
-   * Polynomial 0x31 (x8 + x5 +x4 +1)
-   * Final XOR 0x00
-   */
-
-      constexpr uint8_t POLYNOMIAL(0x31);
-      uint8_t crc(0xFF);
-
-      for (int j = len; j; --j)
-      {
-        crc ^= *data++;
-
-        for (int i = 8; i; --i)
-        {
-          crc = (crc & 0x80) ? (crc << 1) ^ POLYNOMIAL : (crc << 1);
-        }
-      }
-      return crc;
-    }
+    static uint8_t crc8(const uint8_t *data, const int len);
   };
-} // namespace SHT3X
+}; // namespace SHT3X
