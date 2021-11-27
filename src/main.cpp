274fd9aa57b968c5209f72d1327d374b2d1041bc
estshorter <1430311+estshorter@users.noreply.github.com>
2021-11-27 20:45:49 +0900

    formatting using clang-format

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,23 @@
-#include <M5EPD.h>
+#undef ARDUINO_M5STACK_FIRE
+#define ARDUINO_M5STACK_Paper
 #include <ArduinoOTA.h>
-#define FASTLED_INTERNAL //suppress pragma message
+#include <M5EPD.h>
+
+#include <array>
+#define FASTLED_INTERNAL  // suppress pragma message
 #include <FastLED.h>
+
 #include "SHT3X.h"
 #include "WiFiInfo.h"
 
 #define LGFX_M5PAPER
+#define LGFX_USE_V1
 #include <LovyanGFX.hpp>
-#include "myFont.h"
+#include <LGFX_AUTODETECT.hpp>
+
 #include "misc.h"
+#include "myFont.h"
+#include <ESPmDNS.h>
 
 constexpr float FONT_SIZE_LARGE = 3.0;
 constexpr float FONT_SIZE_SMALL = 1.0;
@@ -21,296 +30,260 @@ rtc_date_t date_ntp{4, 1, 1, 1970};
 TwoWire &wire_portA = Wire1;
 SemaphoreHandle_t xMutex = nullptr;
 SHT3X::SHT3X sht30(wire_portA);
-LGFX gfx;
-CRGB leds[3];
+static LGFX gfx;
+std::array<CRGB,3> leds;
 
-inline int syncNTPTimeJP(void)
-{
-  constexpr auto NTP_SERVER1 = "ntp.nict.jp";
-  constexpr auto NTP_SERVER2 = "time.cloudflare.com";
-  constexpr auto NTP_SERVER3 = "time.google.com";
-  constexpr auto TIME_ZONE = "JST-9";
+inline int syncNTPTimeJP(void) {
+	constexpr auto NTP_SERVER1 = "ntp.nict.jp";
+	constexpr auto NTP_SERVER2 = "time.cloudflare.com";
+	constexpr auto NTP_SERVER3 = "time.google.com";
+	constexpr auto TIME_ZONE = "JST-9";
 
-  auto datetime_setter = [](const tm &datetime) {
-    rtc_time_t time{
-        static_cast<int8_t>(datetime.tm_hour),
-        static_cast<int8_t>(datetime.tm_min),
-        static_cast<int8_t>(datetime.tm_sec)};
-    rtc_date_t date{
-        static_cast<int8_t>(datetime.tm_wday),
-        static_cast<int8_t>(datetime.tm_mon + 1),
-        static_cast<int8_t>(datetime.tm_mday),
-        static_cast<int16_t>(datetime.tm_year + 1900)};
+	auto datetime_setter = [](const tm &datetime) {
+		rtc_time_t time{static_cast<int8_t>(datetime.tm_hour), static_cast<int8_t>(datetime.tm_min),
+						static_cast<int8_t>(datetime.tm_sec)};
+		rtc_date_t date{
+			static_cast<int8_t>(datetime.tm_wday), static_cast<int8_t>(datetime.tm_mon + 1),
+			static_cast<int8_t>(datetime.tm_mday), static_cast<int16_t>(datetime.tm_year + 1900)};
 
-    M5.RTC.setDateTime(date, time);
-    date_ntp = date;
-    time_ntp = time;
-  };
+		M5.RTC.setDateTime(date, time);
+		date_ntp = date;
+		time_ntp = time;
+	};
 
-  return syncNTPTime(datetime_setter, TIME_ZONE, NTP_SERVER1, NTP_SERVER2, NTP_SERVER3);
+	return syncNTPTime(datetime_setter, TIME_ZONE, NTP_SERVER1, NTP_SERVER2, NTP_SERVER3);
 }
 
-void handleBtnPPress(void)
-{
-  xSemaphoreTake(xMutex, portMAX_DELAY);
-  prettyEpdRefresh(gfx);
-  gfx.setTextSize(FONT_SIZE_SMALL);
+void handleBtnPPress(void) {
+	xSemaphoreTake(xMutex, portMAX_DELAY);
+	prettyEpdRefresh(gfx);
+	gfx.setTextSize(FONT_SIZE_SMALL);
 
-  gfx.startWrite();
-  gfx.setCursor(0, 0);
-  if (!syncNTPTimeJP())
-  {
-    gfx.println("Succeeded to sync time");
-    struct tm timeInfo;
-    if (getLocalTime(&timeInfo))
-    {
-      gfx.print("getLocalTime:");
-      gfx.println(&timeInfo, "%Y/%m/%d %H:%M:%S");
-    }
-  }
-  else
-  {
-    gfx.println("Failed to sync time");
-  }
+	gfx.startWrite();
+	gfx.setCursor(0, 0);
+	if (!syncNTPTimeJP()) {
+		gfx.println("Succeeded to sync time");
+		struct tm timeInfo;
+		if (getLocalTime(&timeInfo)) {
+			gfx.print("getLocalTime:");
+			gfx.println(&timeInfo, "%Y/%m/%d %H:%M:%S");
+		}
+	} else {
+		gfx.println("Failed to sync time");
+	}
 
-  rtc_date_t date;
-  rtc_time_t time;
+	rtc_date_t date;
+	rtc_time_t time;
 
-  // Get RTC
-  M5.RTC.getDateTime(date, time);
-  gfx.print("RTC         :");
-  gfx.printf("%04d/%02d/%02d ", date.year, date.mon, date.day);
-  gfx.printf("%02d:%02d:%02d", time.hour, time.min, time.sec);
-  gfx.endWrite();
+	// Get RTC
+	M5.RTC.getDateTime(date, time);
+	gfx.print("RTC         :");
+	gfx.printf("%04d/%02d/%02d ", date.year, date.mon, date.day);
+	gfx.printf("%02d:%02d:%02d", time.hour, time.min, time.sec);
+	gfx.endWrite();
 
-  delay(1000);
+	delay(1000);
 
-  gfx.setTextSize(FONT_SIZE_LARGE);
-  xSemaphoreGive(xMutex);
+	gfx.setTextSize(FONT_SIZE_LARGE);
+	xSemaphoreGive(xMutex);	
 }
 
-inline void handleBtnRPress(void)
-{
-  xSemaphoreTake(xMutex, portMAX_DELAY);
-  prettyEpdRefresh(gfx);
-  xSemaphoreGive(xMutex);
+inline void handleBtnRPress(void) {
+	xSemaphoreTake(xMutex, portMAX_DELAY);
+	prettyEpdRefresh(gfx);
+	xSemaphoreGive(xMutex);
 }
 
-void handleBtnLPress(void)
-{
-  xSemaphoreTake(xMutex, portMAX_DELAY);
-  prettyEpdRefresh(gfx);
-  gfx.setCursor(0, 0);
-  gfx.setTextSize(FONT_SIZE_SMALL);
-  gfx.print("Good bye..");
-  gfx.waitDisplay();
-  M5.disableEPDPower();
-  M5.disableEXTPower();
-  M5.disableMainPower();
-  esp_deep_sleep_start();
-  while (true)
-    ;
-  xSemaphoreGive(xMutex);
+void handleBtnLPress(void) {
+	xSemaphoreTake(xMutex, portMAX_DELAY);
+	prettyEpdRefresh(gfx);
+	gfx.setCursor(0, 0);
+	gfx.setTextSize(FONT_SIZE_SMALL);
+	gfx.print("Good bye..");
+	gfx.waitDisplay();
+	M5.disableEPDPower();
+	M5.disableEXTPower();
+	M5.disableMainPower();
+	esp_deep_sleep_start();
+	while (true)
+		;
+	xSemaphoreGive(xMutex);
 }
 
-void handleButton(void *pvParameters)
-{
-  while (true)
-  {
-    delay(500);
-    M5.update();
-    if (M5.BtnP.isPressed())
-    {
-      handleBtnPPress();
-    }
-    else if (M5.BtnR.isPressed())
-    {
-      handleBtnRPress();
-    }
-    else if (M5.BtnL.isPressed())
-    {
-      handleBtnLPress();
-    }
-  }
+void handleButton(void *pvParameters) {
+	while (true) {
+		delay(500);
+		M5.update();
+		if (M5.BtnP.isPressed()) {
+			handleBtnPPress();
+		} else if (M5.BtnR.isPressed()) {
+			handleBtnRPress();
+		} else if (M5.BtnL.isPressed()) {
+			handleBtnLPress();
+		}
+	}
 }
 
-void setup(void)
-{
-  constexpr uint_fast16_t WIFI_CONNECT_RETRY_MAX = 60; // 10 = 5s
-  constexpr uint_fast16_t WAIT_ON_FAILURE = 2000;
+void setup(void) {
+	constexpr uint_fast16_t WIFI_CONNECT_RETRY_MAX = 60;  // 10 = 5s
+	constexpr uint_fast16_t WAIT_ON_FAILURE = 2000;
 
-  M5.begin(true, false, true, true, true, true);
-  WiFi.begin(WiFiInfo::SSID, WiFiInfo::PASS);
+	M5.begin(true, false, true, true, true, true);
+	WiFi.begin(WiFiInfo::SSID, WiFiInfo::PASS);
 
-  FastLED.addLeds<WS2811, 26, GRB>(leds, 3).setCorrection(TypicalSMD5050);
-  FastLED.setBrightness(5);
+	FastLED.addLeds<WS2811, 26, GRB>(leds.data(), 3).setCorrection(TypicalSMD5050);
+	FastLED.setBrightness(5);
 
-  gfx.init();
-  gfx.setEpdMode(epd_mode_t::epd_fast);
-  gfx.setRotation(1);
-  // gfx.setFont(&fonts::lgfxJapanGothic_40);
-  gfx.setFont(&myFont::myFont);
-  gfx.setTextSize(FONT_SIZE_SMALL);
+	gfx.init();
+	gfx.setEpdMode(epd_mode_t::epd_fast);
+	gfx.setRotation(1);
+	// gfx.setFont(&fonts::lgfxJapanGothic_40);
+	gfx.setFont(&myFont::myFont);
+	gfx.setTextSize(FONT_SIZE_SMALL);
 
-  gfx.print("Connecting to Wi-Fi network");
-  for (int cnt_retry = 0;
-       cnt_retry < WIFI_CONNECT_RETRY_MAX && !WiFi.isConnected();
-       cnt_retry++)
-  {
-    delay(500);
-    gfx.print(".");
-  }
-  gfx.println("");
-  if (WiFi.isConnected())
-  {
-    gfx.print("Local IP: ");
-    gfx.println(WiFi.localIP());
-  }
-  else
-  {
-    gfx.println("Failed to connect to a Wi-Fi network");
-    delay(WAIT_ON_FAILURE);
-  }
+	gfx.print("Connecting to Wi-Fi network");
+	for (int cnt_retry = 0; cnt_retry < WIFI_CONNECT_RETRY_MAX && !WiFi.isConnected();
+		 cnt_retry++) {
+		delay(500);
+		gfx.print(".");
+	}
+	gfx.println("");
+	if (WiFi.isConnected()) {
+		gfx.print("Local IP: ");
+		gfx.println(WiFi.localIP());
+		MDNS.begin("m5paper");
+	} else {
+		gfx.println("Failed to connect to a Wi-Fi network");
+		delay(WAIT_ON_FAILURE);
+	}
 
-  ArduinoOTA
-      .onStart([]() {
-        String type;
-        if (ArduinoOTA.getCommand() == U_FLASH)
-          type = "sketch";
-        else // U_SPIFFS
-          type = "filesystem";
+	ArduinoOTA
+		.onStart([]() {
+			String type;
+			if (ArduinoOTA.getCommand() == U_FLASH)
+				type = "sketch";
+			else  // U_SPIFFS
+				type = "filesystem";
 
-        // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-        Serial.println("Start updating " + type);
-      })
-      .onEnd([]() {
-        Serial.println("\nEnd");
-      })
-      .onProgress([](unsigned int progress, unsigned int total) {
-        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-      })
-      .onError([](ota_error_t error) {
-        Serial.printf("Error[%u]: ", error);
-        if (error == OTA_AUTH_ERROR)
-          Serial.println("Auth Failed");
-        else if (error == OTA_BEGIN_ERROR)
-          Serial.println("Begin Failed");
-        else if (error == OTA_CONNECT_ERROR)
-          Serial.println("Connect Failed");
-        else if (error == OTA_RECEIVE_ERROR)
-          Serial.println("Receive Failed");
-        else if (error == OTA_END_ERROR)
-          Serial.println("End Failed");
-      });
+			// NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+			Serial.println("Start updating " + type);
+		})
+		.onEnd([]() { Serial.println("\nEnd"); })
+		.onProgress([](unsigned int progress, unsigned int total) {
+			Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+		})
+		.onError([](ota_error_t error) {
+			Serial.printf("Error[%u]: ", error);
+			if (error == OTA_AUTH_ERROR)
+				Serial.println("Auth Failed");
+			else if (error == OTA_BEGIN_ERROR)
+				Serial.println("Begin Failed");
+			else if (error == OTA_CONNECT_ERROR)
+				Serial.println("Connect Failed");
+			else if (error == OTA_RECEIVE_ERROR)
+				Serial.println("Receive Failed");
+			else if (error == OTA_END_ERROR)
+				Serial.println("End Failed");
+		});
 
-  ArduinoOTA.begin();
+	ArduinoOTA.begin();
 
-  // env2 unit
-  if (!sht30.begin(25, 32, 400000))
-  {
-    gfx.println("Failed to initialize external I2C");
-  }
+	// env2 unit
+	if (!sht30.begin(25, 32, 400000)) {
+		gfx.println("Failed to initialize external I2C");
+	}
 
-  xMutex = xSemaphoreCreateMutex();
-  if (xMutex != nullptr)
-  {
-    xSemaphoreGive(xMutex);
-    xTaskCreatePinnedToCore(handleButton, "handleButton", 4096, nullptr, 1, nullptr, 1);
-  }
-  else
-  {
-    gfx.println("Failed to create a task for buttons");
-  }
-  gfx.println("Init done");
-  delay(1000);
-  gfx.setTextSize(FONT_SIZE_LARGE);
-  prettyEpdRefresh(gfx);
-  gfx.setCursor(0, 0);
+	xMutex = xSemaphoreCreateMutex();
+	if (xMutex != nullptr) {
+		xSemaphoreGive(xMutex);
+		xTaskCreatePinnedToCore(handleButton, "handleButton", 4096, nullptr, 1, nullptr, 1);
+	} else {
+		gfx.println("Failed to create a task for buttons");
+	}
+	gfx.println("Init done");
+	delay(1000);
+	gfx.setTextSize(FONT_SIZE_LARGE);
+	prettyEpdRefresh(gfx);
+	gfx.setCursor(0, 0);
 }
 
-void loop(void)
-{
-  constexpr uint_fast16_t SLEEP_SEC = 5;
-  constexpr uint_fast32_t TIME_SYNC_CYCLE = 7 * 3600 * 24 / SLEEP_SEC;
+void loop(void) {
+	constexpr uint_fast16_t SLEEP_SEC = 5;
+	constexpr uint_fast32_t TIME_SYNC_CYCLE = 3600 * 24 / SLEEP_SEC;
 
-  static uint32_t cnt = 0;
+	static uint32_t cnt = 0;
 
-  xSemaphoreTake(xMutex, portMAX_DELAY);
-  ArduinoOTA.handle();
+	xSemaphoreTake(xMutex, portMAX_DELAY);
+	ArduinoOTA.handle();
 
-  float tmp = 0.0;
-  uint_fast8_t hum = 0;
+	float tmp = 0.0;
+	uint_fast8_t hum = 0;
 
-  if (!sht30.read())
-  {
-    tmp = sht30.getTemperature();
-    hum = sht30.getHumidity();
-  }
-  auto co2 = getCo2Data();
-  setLEDColor(leds, co2);
+	if (!sht30.read()) {
+		tmp = sht30.getTemperature();
+		hum = sht30.getHumidity();
+	}
+	auto co2 = getCo2Data();
+	setLEDColor(leds, co2);
 
-  rtc_date_t date;
-  rtc_time_t time;
+	rtc_date_t date;
+	rtc_time_t time;
 
-  M5.RTC.getDateTime(date, time);
+	M5.RTC.getDateTime(date, time);
 
-  gfx.startWrite();
-  gfx.fillScreen(TFT_WHITE);
-  gfx.fillRect(0.57 * M5PAPER_SIZE_LONG_SIDE, 0, 3, M5PAPER_SIZE_SHORT_SIDE, TFT_BLACK);
+	gfx.startWrite();
+	gfx.fillScreen(TFT_WHITE);
+	gfx.fillRect(0.57 * M5PAPER_SIZE_LONG_SIDE, 0, 3, M5PAPER_SIZE_SHORT_SIDE, TFT_BLACK);
 
-  constexpr uint_fast16_t offset_y = 30;
-  constexpr uint_fast16_t offset_x = 45;
+	constexpr uint_fast16_t offset_y = 30;
+	constexpr uint_fast16_t offset_x = 45;
 
-  gfx.setCursor(0, offset_y);
-  gfx.setClipRect(offset_x, offset_y, M5PAPER_SIZE_LONG_SIDE - offset_x, M5PAPER_SIZE_SHORT_SIDE - offset_y);
-  gfx.printf("%02d:%02d:%02d\r\n", time.hour, time.min, time.sec);
-  gfx.printf("%04dppm\r\n", co2);
-  gfx.printf("%02.1f℃\r\n", tmp);
-  gfx.printf("%0d%%", hum);
-  gfx.clearClipRect();
+	gfx.setCursor(0, offset_y);
+	gfx.setClipRect(offset_x, offset_y, M5PAPER_SIZE_LONG_SIDE - offset_x,
+					M5PAPER_SIZE_SHORT_SIDE - offset_y);
+	gfx.printf("%02d:%02d:%02d\r\n", time.hour, time.min, time.sec);
+	gfx.printf("%04dppm\r\n", co2);
+	gfx.printf("%02.1f℃\r\n", tmp);
+	gfx.printf("%0d%%", hum);
+	gfx.clearClipRect();
 
-  constexpr float x = 0.61 * M5PAPER_SIZE_LONG_SIDE;
-  gfx.setCursor(0, offset_y);
-  gfx.setClipRect(x, offset_y, M5PAPER_SIZE_LONG_SIDE - offset_x - x, M5PAPER_SIZE_SHORT_SIDE - offset_y);
-  gfx.printf("%04d\r\n", date.year);
-  gfx.printf("%02d/%02d\r\n", date.mon, date.day);
-  gfx.println(weekdayToString(date.week));
-  gfx.clearClipRect();
+	constexpr float x = 0.61 * M5PAPER_SIZE_LONG_SIDE;
+	gfx.setCursor(0, offset_y);
+	gfx.setClipRect(x, offset_y, M5PAPER_SIZE_LONG_SIDE - offset_x - x,
+					M5PAPER_SIZE_SHORT_SIDE - offset_y);
+	gfx.printf("%04d\r\n", date.year);
+	gfx.printf("%02d/%02d\r\n", date.mon, date.day);
+	gfx.println(weekdayToString(date.week));
+	gfx.clearClipRect();
 
-  constexpr float offset_y_info = 0.75 * M5PAPER_SIZE_SHORT_SIDE;
-  gfx.setCursor(0, offset_y_info);
-  gfx.setTextSize(FONT_SIZE_SMALL);
-  gfx.setClipRect(x, offset_y_info, M5PAPER_SIZE_LONG_SIDE - x, gfx.height() - offset_y_info);
-  gfx.print("WiFi: ");
-  gfx.println(WiFiConnectedToString());
+	constexpr float offset_y_info = 0.75 * M5PAPER_SIZE_SHORT_SIDE;
+	gfx.setCursor(0, offset_y_info);
+	gfx.setTextSize(FONT_SIZE_SMALL);
+	gfx.setClipRect(x, offset_y_info, M5PAPER_SIZE_LONG_SIDE - x, gfx.height() - offset_y_info);
+	gfx.print("WiFi: ");
+	gfx.println(WiFiConnectedToString());
 
-  constexpr uint32_t low = 3300;
-  constexpr uint32_t high = 4350;
+	constexpr uint32_t low = 3300;
+	constexpr uint32_t high = 4350;
 
-  auto vol = std::min(std::max(M5.getBatteryVoltage(), low), high);
-  gfx.printf("BAT : %04dmv\r\n", vol);
-  gfx.print("NTP : ");
-  if (date_ntp.year == 1970)
-  {
-    gfx.print("YET"); // not initialized
-  }
-  else
-  {
-    gfx.printf("%02d/%02d %02d:%02d",
-               date_ntp.mon, date_ntp.day,
-               time_ntp.hour, time_ntp.min);
-  }
+	auto vol = std::min(std::max(M5.getBatteryVoltage(), low), high);
+	gfx.printf("BAT : %04dmv\r\n", vol);
+	gfx.print("NTP : ");
+	if (date_ntp.year == 1970) {
+		gfx.print("YET");  // not initialized
+	} else {
+		gfx.printf("%02d/%02d %02d:%02d", date_ntp.mon, date_ntp.day, time_ntp.hour, time_ntp.min);
+	}
 
-  gfx.clearClipRect();
-  gfx.setTextSize(FONT_SIZE_LARGE);
-  gfx.endWrite();
+	gfx.clearClipRect();
+	gfx.setTextSize(FONT_SIZE_LARGE);
+	gfx.endWrite();
 
-  cnt++;
-  if (cnt == TIME_SYNC_CYCLE)
-  {
-    syncNTPTimeJP();
-    cnt = 0;
-  }
-  xSemaphoreGive(xMutex);
-  delay(SLEEP_SEC * 1000);
+	cnt++;
+	if (cnt == TIME_SYNC_CYCLE) {
+		syncNTPTimeJP();
+		cnt = 0;
+	}
+	xSemaphoreGive(xMutex);
+	delay(SLEEP_SEC * 1000);
 }
--- src/misc.h
+++ src/misc.h
@@ -1,145 +1,120 @@
 #pragma once
 
-#include <functional>
-#include <HTTPClient.h>
-#include <ArduinoJson.hpp>
 #include <FastLED.h>
+#include <HTTPClient.h>
+
+#include <ArduinoJson.hpp>
+#include <functional>
+
 #include "esp_sntp.h"
 
-#include <LovyanGFX.hpp>
-
-inline String WiFiConnectedToString(void)
-{
-    return WiFi.isConnected() ? String("OK") : String("NG");
+inline String WiFiConnectedToString(void) {
+	return WiFi.isConnected() ? String("OK") : String("NG");
 }
 
-String weekdayToString(const int8_t weekDay)
-{
-    switch (weekDay)
-    {
-    case 0:
-        return String("日");
-    case 1:
-        return String("月");
-    case 2:
-        return String("火");
-    case 3:
-        return String("水");
-    case 4:
-        return String("木");
-    case 5:
-        return String("金");
-    case 6:
-        return String("土");
-    }
-    return String("");
+String weekdayToString(const int8_t weekDay) {
+	switch (weekDay) {
+		case 0:
+			return String("日");
+		case 1:
+			return String("月");
+		case 2:
+			return String("火");
+		case 3:
+			return String("水");
+		case 4:
+			return String("木");
+		case 5:
+			return String("金");
+		case 6:
+			return String("土");
+	}
+	return String("");
 }
 
-uint_fast16_t getCo2Data(void)
-{
-    using namespace ArduinoJson;
-    constexpr auto CO2_DATA_URL = "http://192.168.10.105/api/data";
-    constexpr uint16_t HTTP_TIMEOUT = 3000;
+uint_fast16_t getCo2Data(void) {
+	using namespace ArduinoJson;
+	constexpr auto CO2_DATA_URL = "http://192.168.10.103/api/data";
+	constexpr uint16_t HTTP_TIMEOUT = 3000;
 
-    if (!WiFi.isConnected())
-        return 0;
+	if (!WiFi.isConnected()) return 0;
 
-    WiFiClient client;
-    HTTPClient http;
-    if (!http.begin(client, CO2_DATA_URL))
-    {
-        Serial.printf("[HTTP] Failed to parse url\n");
-        return 0;
-    }
-    http.setTimeout(HTTP_TIMEOUT);
+	WiFiClient client;
+	HTTPClient http;
+	if (!http.begin(client, CO2_DATA_URL)) {
+		Serial.printf("[HTTP] Failed to parse url\n");
+		return 0;
+	}
+	http.setTimeout(HTTP_TIMEOUT);
 
-    int httpCode = http.GET();
-    if (httpCode != HTTP_CODE_OK)
-    {
-        Serial.printf("[HTTP] GET... failed, error: %s\n",
-                      http.errorToString(httpCode).c_str());
-        http.end();
-        return 0;
-    }
-    StaticJsonDocument<64> filter;
-    filter["co2"]["value"] = true;
+	int httpCode = http.GET();
+	if (httpCode != HTTP_CODE_OK) {
+		Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+		http.end();
+		return 0;
+	}
+	StaticJsonDocument<64> filter;
+	filter["co2"]["value"] = true;
 
-    StaticJsonDocument<64> doc;
-    auto err = deserializeJson(doc, client, DeserializationOption::Filter(filter));
-    http.end();
-    if (err)
-    {
-        Serial.printf("[JSON] DeserializationError, error: %s\n", err.c_str());
-        return 0;
-    }
+	StaticJsonDocument<64> doc;
+	auto err = deserializeJson(doc, client, DeserializationOption::Filter(filter));
+	http.end();
+	if (err) {
+		Serial.printf("[JSON] DeserializationError, error: %s\n", err.c_str());
+		return 0;
+	}
 
-    return doc["co2"]["value"];
+	return doc["co2"]["value"];
 }
 
-void setLEDColor(CRGB *leds, const uint_fast16_t co2)
-{
-    constexpr uint_fast8_t ID_LED_USE = 1;
-    leds[0] = CRGB::Black;
-    leds[2] = CRGB::Black;
-    if (co2 < 600)
-    {
-        leds[ID_LED_USE] = CRGB::White;
-    }
-    else if (co2 < 1200)
-    {
-        leds[ID_LED_USE] = CRGB::Green;
-    }
-    else if (co2 < 1500)
-    {
-        leds[ID_LED_USE] = CRGB::Yellow;
-    }
-    else if (co2 < 2000)
-    {
-        leds[ID_LED_USE] = CRGB::Red;
-    }
-    else
-    {
-        leds[ID_LED_USE] = CRGB::Red;
-        leds[0] = CRGB::Red;
-        leds[2] = CRGB::Red;
-    }
-    FastLED.show();
+void setLEDColor(std::array<CRGB, 3> &leds, const uint_fast16_t co2) {
+	constexpr uint_fast8_t ID_LED_USE = 1;
+	leds[0] = CRGB::Black;
+	leds[2] = CRGB::Black;
+	if (co2 < 600) {
+		leds[ID_LED_USE] = CRGB::White;
+	} else if (co2 < 1200) {
+		leds[ID_LED_USE] = CRGB::Green;
+	} else if (co2 < 1500) {
+		leds[ID_LED_USE] = CRGB::Yellow;
+	} else if (co2 < 2000) {
+		leds[ID_LED_USE] = CRGB::Red;
+	} else {
+		leds[ID_LED_USE] = CRGB::Red;
+		leds[0] = CRGB::Red;
+		leds[2] = CRGB::Red;
+	}
+	FastLED.show();
 }
 
-inline void prettyEpdRefresh(LGFX &gfx)
-{
-    gfx.setEpdMode(epd_mode_t::epd_quality);
-    gfx.fillScreen(TFT_WHITE);
-    gfx.setEpdMode(epd_mode_t::epd_fast);
+inline void prettyEpdRefresh(LGFX &gfx) {
+	gfx.setEpdMode(epd_mode_t::epd_quality);
+	gfx.fillScreen(TFT_WHITE);
+	gfx.setEpdMode(epd_mode_t::epd_fast);
 }
 
-int syncNTPTime(std::function<void(const tm &)> datetimeSetter, const char *tz,
-                const char *server1, const char *server2 = nullptr, const char *server3 = nullptr)
-{
-    if (!WiFi.isConnected())
-    {
-        return 1;
-    }
+int syncNTPTime(std::function<void(const tm &)> datetimeSetter, const char *tz, const char *server1,
+				const char *server2 = nullptr, const char *server3 = nullptr) {
+	if (!WiFi.isConnected()) {
+		return 1;
+	}
 
-    configTzTime(tz, server1, server2, server3);
+	configTzTime(tz, server1, server2, server3);
 
-    // https://github.com/espressif/esp-idf/blob/master/examples/protocols/sntp/main/sntp_example_main.c
-    int retry = 0;
-    constexpr int retry_count = 50;
-    while (sntp_get_sync_status() != SNTP_SYNC_STATUS_COMPLETED && ++retry < retry_count)
-    {
-        delay(100);
-    }
-    if (retry == retry_count)
-    {
-        return 1;
-    }
+	// https://github.com/espressif/esp-idf/blob/master/examples/protocols/sntp/main/sntp_example_main.c
+	int retry = 0;
+	constexpr int retry_count = 50;
+	while (sntp_get_sync_status() != SNTP_SYNC_STATUS_COMPLETED && ++retry < retry_count) {
+		delay(100);
+	}
+	if (retry == retry_count) {
+		return 1;
+	}
 
-    struct tm datetime;
-    if (!getLocalTime(&datetime))
-        return 1;
+	struct tm datetime;
+	if (!getLocalTime(&datetime)) return 1;
 
-    datetimeSetter(datetime);
+	datetimeSetter(datetime);
 
-    return 0;
+	return 0;
 }
